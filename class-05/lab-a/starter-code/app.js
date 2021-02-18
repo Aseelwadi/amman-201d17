c9a32a5ef699e80ecb8d29e0920aa2d224d5f91e
Aseelwadi <aseelalsqoor23@gmail.com>
2021-02-18 04:26:34 -0800

    q5 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -72,25 +72,22 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-    let sum = 0;
-for (let i = 0; i < testArray.length; i++) {
-  sum += testArray[i];
-}
- let statment =`${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sum} is their sum.`;
-return [sum,statment];
-}
+  }
 
 
 
 // Here is the test for sumArray(); uncomment it to run it
 
- testSumArray(testArray);
+ //testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() 
+that takes an array of numbers as its argument and returns an array whose first element is the product 
+of those numbers, and the second element is a string that EXACTLY
+ follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
@@ -99,12 +96,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
+
+let testArray2 = [2, 3, 4];
 function multiplyArray(multArr) { //eslint-disable-line
+
+    let muliply = testArray[0] * testArray[1] * testArray[2];
+
+    let statment =`The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${multiply}.`;
+return [multiply,statment];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
